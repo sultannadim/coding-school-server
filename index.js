3bf5372dc1aaf16b7c57b80e15dd8253437d8299
sultan nadim <sultan5218@gmail.com>
2022-10-25 15:39:39 +0600

    category json added

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+const categories = require("./data/category.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Api comming soon");
+});
+
+app.get("/categories", (req, res) => {
+  res.send(categories);
 });
 
 app.listen(port, () => {
