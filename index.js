40201a3d849ef07e89486ca80c71a11fcee2f175
sultan nadim <sultan5218@gmail.com>
2022-10-25 16:39:58 +0600

    server complete

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const categories = require("./data/category.json");
+const courseDetails = require("./data/details.json");
 
 app.use(cors());
 
@@ -10,8 +11,23 @@ app.get("/", (req, res) => {
   res.send("Api comming soon");
 });
 
+app.get("/courses", (req, res) => {
+  res.send(courseDetails);
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const details = courseDetails.find((d) => d.id == id);
+  res.send(details);
+});
+
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const categoryCourse = courseDetails.filter((d) => d.id == id);
+  res.send(categoryCourse);
 });
 
 app.listen(port, () => {
